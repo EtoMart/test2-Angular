e68212f19bd86d1dea4850a2889b4b3b734ff51e
EtoMart <69122792+EtoMart@users.noreply.github.com>
2021-12-03 02:36:22 +0300

    v 0.9

--- dcl/src/app/app.component.ts
+++ dcl/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DriverComponentComponent } from './modules/driver-module/driver-component/driver-component.component';
 
 @Component({
@@ -11,13 +10,12 @@ export class AppComponent {
   constructor() {
   }
 
-  form: FormGroup;
-  @ViewChild(DriverComponentComponent)
-  public driverComponent: DriverComponentComponent;
+  @ViewChild(DriverComponentComponent, {static: true})
+  public driverComponent: DriverComponentComponent | undefined;
 
-  submit(): void {
-    console.log('form submitted ', this.form);
-    const formData = { ...this.form.value };
+  public submit(): void {
+    console.log('form submitted ', this.driverComponent?.parentForm);
+    const formData = { ...this.driverComponent?.parentForm.value };
     console.log('Form value:', formData);
   }
 }
